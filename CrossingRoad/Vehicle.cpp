0fad6f4d39151e1f3792f15e129f24c55e0889c4
tahoangquan2 <hoangquandt69@gmail.com>
2023-11-18 20:04:18 +0700

    Format files

--- CrossingRoad/Vehicle.cpp
+++ CrossingRoad/Vehicle.cpp
@@ -6,27 +6,33 @@ Vehicle::Vehicle(float xPos, float yPos, std::string type)
 Vehicle::Vehicle()
     : xPos(0), yPos(0), type("") {}
 
-float Vehicle::getXPosition() const {
+float Vehicle::getXPosition() const
+{
     return xPos;
 }
 
-float Vehicle::getYPosition() const {
+float Vehicle::getYPosition() const
+{
     return yPos;
 }
 
-std::string Vehicle::getType() const {
+std::string Vehicle::getType() const
+{
     return type;
 }
 
-void Vehicle::updatePosition(float speed) {
+void Vehicle::updatePosition(float speed)
+{
     xPos += speed;
 }
 
-void Vehicle::rotatePosition(bool gotoBack) {
+void Vehicle::rotatePosition(bool gotoBack)
+{
     if (gotoBack == true) {
         xPos += 1920;
     }
-    else {
+    else
+    {
         xPos -= 1920;
     }
 }
