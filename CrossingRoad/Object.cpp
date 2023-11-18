b56c831d0d1e59691367d11f2c7f399d306ae2d6
TriSi51 <ngotrisikhtn2022@gmail.com>
2023-11-18 22:18:20 +0700

    new_update

--- CrossingRoad/Object.cpp
+++ CrossingRoad/Object.cpp
@@ -1,24 +1,76 @@
 
 #include "Object.h"
-
-Stableobject::Stableobject(const float& x,const float& y, string filename, const float& width, const float& height) {
-	this->x = x;
-	this->y = y;
-	if (!texture.loadFromFile(filename))
-	{
-		cout << "Failed to load grass spritesheet!" << endl;
-		exit(0);
-	}
-
-	IntRect rectangle(x, y, width, height);
-	sprite.setTexture(texture);
-	sprite.setTextureRect(rectangle);
+FloatRect StableObject::getglobalBounds() {
+	return sprite.getGlobalBounds();
 }
 
-void Stableobject::draw(RenderWindow& window) {
+bool StableObject::doesIntersect( StableObject *other)  {
+	return this->sprite.getGlobalBounds().intersects(other->sprite.getGlobalBounds());
+}
+void StableObject::draw(RenderWindow& window) {
 	window.draw(sprite);
 }
+
+//void Tree::draw(RenderWindow& window) {
+//	window.draw(sprite);
+//}
+//
+//FloatRect Tree::getglobalBounds() {
+//		return sprite.getGlobalBounds();
+//}
+//
+//bool Tree::doesIntersect(  Stableobject& other) {
+//	
+//	return this->sprite.getGlobalBounds().intersects(other.getglobalBounds());
+//}
+//
+//
+// void Bigtree::draw(RenderWindow& window) {
+//	 window.draw(sprite);
+// }
+// FloatRect Bigtree::getglobalBounds() {
+//	 return sprite.getGlobalBounds();
+// }
+// bool Bigtree::doesIntersect(Stableobject& other) {
+//
+//	 return this->sprite.getGlobalBounds().intersects(other.getglobalBounds());
+// }
 
 void Lawn::draw(RenderWindow& window){
 	window.draw(sprite);
 }
+
+void Lawn::drawobject(RenderWindow& window) {
+	for (const auto& object : stableobject) {
+		object->draw(window);
+	}
+}
+
+void Lawn::randomobejct(RenderWindow& window) {
+	bool stop = false;
+	while (!stop) {
+		float randomX = static_cast<float>(rand() % static_cast<int>(width));
+		float randomY = y + static_cast<float>(rand() % static_cast<int>(height));
+		std::srand(static_cast<unsigned>(std::time(0)));
+		int choice= 1 + std::rand() % 2;
+		
+		StableObject* arr = nullptr;
+		if (choice == 1) {
+			arr = new StableObject(randomX, randomY, Tree);
+		}
+		else if (choice == 2) {
+			arr = new StableObject(randomX, randomY, Bigtree);
+		}
+	
+		for (int i = 0; i < stableobject.size(); i++) {
+			if (arr->doesIntersect(stableobject[i]) ){
+				stableobject.push_back(arr);
+				stop = true;
+				break;
+			}
+		}
+	}
+	
+
+}
+
--- CrossingRoad/Object.h
+++ CrossingRoad/Object.h
@@ -1,20 +1,99 @@
 #pragma once
 #include <vector>
 #include "character.h"
-class Stableobject {
+enum typeobj {
+	Tree, Bigtree
+};
+
+class StableObject {
 private:
-	float x;
-	float y;
-	float width;
-	float height;
+	float PositionX;
+	float PositionY;
 	Texture texture;
 	Sprite sprite;
 public:
-	Stableobject(const float& x, const float& y, string filename, const float& width, const float& height);
-	void draw(RenderWindow& window);
+	StableObject(float positionX, float positionY,const typeobj& type) : PositionX(positionX), PositionY(positionY) {
+		string filename= "Content/Image/basic_grass.png";
+		float x, y, width, height;
+		if (type == Tree) {
+			 x = 0; y = 0; width = 16; height = 32;
+		}
+		else if (type == Bigtree) {
+			x = 16; y = 0; width = 32; height = 32;
+		}
+		if (!texture.loadFromFile(filename))
+		{
+			cout << "Failed to load grass spritesheet!" << endl;
+			exit(0);
+		}
+		IntRect rectangle(x, y, width, height);
+		sprite.setTexture(texture);
+		sprite.setTextureRect(rectangle);
+		sprite.setPosition(Vector2f(positionX, positionY));
+	}
+
+	FloatRect getglobalBounds();
+	bool doesIntersect( StableObject *other)  ;
+	void draw(RenderWindow& window) ;
 };
+//class Stableobject {
+//public:
+//	virtual FloatRect getglobalBounds() = 0;
+//	virtual bool doesIntersect(  Stableobject& other) = 0;
+//	virtual void draw(RenderWindow& window)=0;
+//	
+//};
+//
+//class Tree: public Stableobject {
+//private:
+//	Texture texture;
+//	Sprite sprite;
+//public:
+//	Tree(float positionX, float positionY)  {
+//		string filename = "Content/Image/basic_grass.png";
+//		float x = 0, y = 0, width = 16, height=32;
+//
+//		if (!texture.loadFromFile(filename))
+//		{
+//			cout << "Failed to load grass spritesheet!" << endl;
+//			exit(0);
+//		}
+//
+//		IntRect rectangle(x, y, width, height);
+//		sprite.setTexture(texture);
+//		sprite.setTextureRect(rectangle);
+//		sprite.setPosition(Vector2f(positionX, positionY));
+//	}
+//	FloatRect getglobalBounds() override;
+//	bool doesIntersect( Stableobject& other) override;
+//	void draw(RenderWindow& window) override;
+//};
+//class Bigtree : public Stableobject {
+//private:
+//	Texture texture;
+//	Sprite sprite;
+//public:
+//	Bigtree(float positionX, float positionY){
+//		string filename = "Content/Image/basic_grass.png";
+//		if (!texture.loadFromFile(filename))
+//		{
+//			cout << "Failed to load grass spritesheet!" << endl;
+//			exit(0);
+//		}
+//		float x = 16, y = 0, width = 32, height = 32;
+//		IntRect rectangle(x, y, width, height);
+//		sprite.setTexture(texture);
+//		sprite.setTextureRect(rectangle);
+//		sprite.setPosition(Vector2f(positionX, positionY));
+//	}
+//	FloatRect getglobalBounds() override;
+//	bool doesIntersect(Stableobject& other) override;
+//	void draw(RenderWindow& window) override;
+//};
+
+
 class Superlane {
-private:
+protected:
 	float y;
 	float x;
 	float width;
@@ -23,10 +102,12 @@ private:
 public:
 	Superlane(const float& x, const float& y, const float& width, const float& height) : x(x), y(y),width(width),height(height){}
 	virtual void draw(RenderWindow& window)=0;
+	virtual void drawobject(RenderWindow& window) = 0;
+	virtual void randomobejct(RenderWindow& window) = 0;
 };
 class Lawn: public Superlane  {
 private:
-	std::vector<Stableobject*> smallobstacle;
+	std::vector<StableObject*> stableobject;
 	Texture texture;
 	Sprite sprite;
 public:
@@ -40,8 +121,17 @@ public:
 		sprite.setTextureRect(rectangle);
 	}
 	void draw(RenderWindow& window) override;
+	void drawobject(RenderWindow& window) override;
+	void randomobejct(RenderWindow& window) override;
 };
 class Road: public Superlane {
 private:
 	// vehicle class 
+};
+
+class Background {
+private:
+	vector<Superlane*> background;
+public:
+	
 };
